269ac693929f56de4cfcbebfc6a8d0509fc75b00
shaunaono <shaunaono@gmail.com>
2018-03-01 18:17:10 -0800

    push2

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,45 +1,33 @@
-var topic = ['baby goats', 'Goldbergs', 'weightlifting'];
+//var topic = ["baby goats", "Goldbergs", "weightlifting"];
 
+$("button").on("click", function() {
+  var giphy = $(this).attr("data-giphy");
 
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=nVJYo50IWxcARwdaRX8Vg2TObE1mCXsw&q= " +
+    giphy +
+    " &limit=10&offset=0&rating=G&lang=en";
 
-$("button").on("click",function(){
-    var giphy = $(this).attr("data-giphy");
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response) {
+    console.log(queryURL);
 
-    var queryURL = https://api.giphy.com/v1/gifs/search?api_key=nVJYo50IWxcARwdaRX8Vg2TObE1mCXsw&q=Goldbergs&limit=10&offset=0&rating=G&lang=en
+    console.log(response);
+    var results = response.data;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+    for (var i = 0; i < results.length; i++) {
+      var giphyDiv = $("<div>");
+      var p = $("<p>").text("Rating: " + results[i].rating);
+      var giphyImage = $("<img>");
 
-    .then(function(response) {
-        console.log(queryURL);
+      giphyImage.attr("src", results[i].images.fixed_height.url);
 
-        console.log(response);
-        var results = response.data;
+      giphyDiv.append(p);
+      giphyDiv.append(giphyImage);
 
-        for (var i = 0; i < results.length; i++){
-
-            var giphyDiv = $("<div>");
-            var p = $("<p>").text("Rating: "+ results[i].rating);
-            var giphyImage = $("<img>");
-
-            giphyImage.attr("src", results[i].images.fixed_height.url);
-
-            giphyDiv.append(p);
-            giphyDiv.append(giphyImage);
-
-            $("#gis-appear-here").prepend(giphyDiv);
-        }
-   
-    });
-
+      $("#gis-appear-here").prepend(giphyDiv);
+    }
+  });
 });
-
-
-
-
-
-
-
-
